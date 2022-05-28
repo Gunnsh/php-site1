fff1370ce8fcd480f7a47278cce3bc5c797e3ab3
Gunnsh <radio_art@mail.ru>
2022-05-28 15:03:59 +0700

    getting started 1

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Metanit.com</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>
+            <?= "Первый сайт на PHP"; ?>
+        </h1>
+        <div>
+            2 + 2 = <?= 2+2 ?>
+        </div>
+    </body>
+<html>
