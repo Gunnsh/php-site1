e7c05474fec3ec3deac25cd8aabd1b0c911c1179
Gunnsh <radio_art@mail.ru>
2022-05-30 19:39:48 +0700

    another day of learning

--- hello.php
+++ hello.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Metanit.com</title>
-        <meta charset="utf-8" />
-    </head>
-    <body>
-        <h1>
-            <?= "Первый сайт на PHP"; ?>
-        </h1>
-        <div>
-            2 + 2 = <?= 2+2 ?>
-        </div>
-    </body>
-<html>
+<head>
+<title>METANIT.COM</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<table>
+<?php
+function sum(...$numbers) // определяем функцию sum с типом данных "массив" по названием numbers
+{
+    $result = 0; // задаем параметр для цикла
+    foreach($numbers as $number) { // задаем цикл массива numbers и переменную number для каждого элемента массива
+        $result += $number; // действие с заданными выше параметрами, сначала плюсуем к 0 number, потом возвращаем результат в переменную
+// действие будет произведено столько раз, сколько элементов в массиве numbers
+    }
+    echo "<p>Сумма: $result</p>"; // выводим на экран конечное значение переменной result
+}
+sum(1, 2, 3); // result(0)+number(1)=result(1)
+ // result(1)+number(2)=result(3)
+ // result(3)+number(3)=result(6), цикл окончен, вывод на экран значение переменной result "Сумма: 6"
+sum(2, 3);
+sum(4, 5, 8, 10);
+?>
+</table>
+</body>
+</html>
